70287d52179214cb57096aa8ce41b627f5d6a260
vdr <rawtevipula25@gmail.com>
2017-07-15 01:19:53 -0400

    Added owl and c

---
+++ xyz1.py
@@ -0,0 +1,43 @@
+jettyrpoehvp q
+pqiwoje rpqoijw
+A
+A
+A
+A
+A
+B
+C
+
+kn vpioejrt- iD
+A
+B
+C
+C
+D
+D
+A
+B
+B
+B
+B
+iiikmr[lakmr[kam[rklmw
+D
+D
+D
+C
+C
+A
+A
+A
+A
+A
+A
+erivmierjv -e
+B
+B
+B
+B
+B
+B
+B
+pakwmepiamwe=rpomq:hey, gedit works!!!!
